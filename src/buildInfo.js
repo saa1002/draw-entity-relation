be15cbe2fecab5e840b76d1fd58645555289e487
steven <saa1002@alu.ubu.es>
2026-05-12 09:29:25 +0200

    test: preserve multivalued attributes during diagram normalization

--- src/buildInfo.js
+++ src/buildInfo.js
@@ -1,1 +1,1 @@
-export const BUILD_DATE = "12/5/2026, 9:13:54";
+export const BUILD_DATE = "12/5/2026, 9:20:01";
--- tests/unit/er/diagramNormalization.test.js
+++ tests/unit/er/diagramNormalization.test.js
@@ -5,7 +5,7 @@ import {
 } from '../../../src/domain/er/diagramNormalization'
 
 describe("Nested attribute normalization", () => {
-    test("Flat attributes should remain unchanged without adding children", () => {
+    test("flat attributes should remain unchanged without adding children", () => {
         const attribute = normalizeAttribute({
             idMx: "attr-1",
             name: "name",
@@ -14,17 +14,47 @@ describe("Nested attribute normalization", () => {
         expect(attribute).not.toHaveProperty("children")
     })
 
-    test.each([
-        ["entity", normalizeAttribute],
-        ["relation", normalizeRelationAttribute],
-    ])("%s attributes should normalize children recursively", (_, normalize) => {
-        const attribute = normalize({
+    test("entity attributes should preserve multivalued flags", () => {
+        const attribute = normalizeAttribute({
+            idMx: "attr-1",
+            name: "phones",
+            multivalued: true,
+        })
+
+        expect(attribute).toMatchObject({
+            idMx: "attr-1",
+            name: "phones",
+            multivalued: true,
+        })
+
+        expect(attribute).not.toHaveProperty("children")
+    })
+
+    test("relation attributes should preserve multivalued flags", () => {
+        const attribute = normalizeRelationAttribute({
+            idMx: "attr-1",
+            name: "phones",
+            multivalued: true,
+        })
+
+        expect(attribute).toMatchObject({
+            idMx: "attr-1",
+            name: "phones",
+            multivalued: true,
+        })
+
+        expect(attribute).not.toHaveProperty("children")
+    })
+
+    test("entity attributes should normalize children recursively", () => {
+        const attribute = normalizeAttribute({
             idMx: "attr-1",
             name: "address",
             children: [
                 {
                     idMx: "attr-2",
                     name: "location",
+                    multivalued: true,
                     children: [
                         {
                             idMx: "attr-3",
@@ -40,6 +70,42 @@ describe("Nested attribute normalization", () => {
             name: "location",
             key: false,
             partialKey: false,
+            multivalued: true,
+        })
+
+        expect(attribute.children[0].children[0]).toMatchObject({
+            idMx: "attr-3",
+            name: "city",
+            key: false,
+            partialKey: false,
+        })
+    })
+
+    test("relation attributes should normalize children recursively", () => {
+        const attribute = normalizeRelationAttribute({
+            idMx: "attr-1",
+            name: "address",
+            children: [
+                {
+                    idMx: "attr-2",
+                    name: "location",
+                    multivalued: true,
+                    children: [
+                        {
+                            idMx: "attr-3",
+                            name: "city",
+                        },
+                    ],
+                },
+            ],
+        })
+
+        expect(attribute.children[0]).toMatchObject({
+            idMx: "attr-2",
+            name: "location",
+            key: false,
+            partialKey: false,
+            multivalued: true,
         })
 
         expect(attribute.children[0].children[0]).toMatchObject({
