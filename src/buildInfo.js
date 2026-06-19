4cc42cd5aa69d6c3a6553bbdbec2c37d84b6c540
steven <saa1002@alu.ubu.es>
2026-06-19 12:34:32 +0200

    docs: update chapters with final usability refinements

--- src/buildInfo.js
+++ src/buildInfo.js
@@ -1,4 +1,4 @@
 // Archivo generado automáticamente por scripts/generateBuildInfo.js
 export const APP_VERSION = "1.0.0";
 export const BUILD_DATE = "19/06/2026";
-export const BUILD_COMMIT = "a93e652";
+export const BUILD_COMMIT = "a72c9fb";
