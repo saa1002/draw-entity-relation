4c29d12cc31dec423fd45de63c4685ed8c8cbd69
steven <saa1002@alu.ubu.es>
2026-06-20 17:01:17 +0200

    docs: align planning requirements and design with final changes

--- src/buildInfo.js
+++ src/buildInfo.js
@@ -1,4 +1,4 @@
 // Archivo generado automáticamente por scripts/generateBuildInfo.js
 export const APP_VERSION = "1.0.0";
 export const BUILD_DATE = "20/06/2026";
-export const BUILD_COMMIT = "4b85a01";
+export const BUILD_COMMIT = "d4f445d";
