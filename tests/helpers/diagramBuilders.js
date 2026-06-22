5cad177a8b6ff5dad15be4c3797a6d754eda3ac9
steven <saa1002@alu.ubu.es>
2026-06-22 12:47:39 +0200

    test: add shared diagram builders for unit tests

---
+++ tests/helpers/diagramBuilders.js
@@ -0,0 +1,170 @@
+import { RELATION_ARITIES } from '../../src/domain/er/relations'
+
+export const createAttribute = ({
+    idMx,
+    name,
+    key = false,
+    partialKey = false,
+    children,
+    ...rest
+}) => ({
+    idMx,
+    name,
+    key,
+    partialKey,
+    ...(children ? { children } : {}),
+    ...rest,
+})
+
+export const createStrongEntity = ({ idMx, name, keyName, attributes }) => ({
+    idMx,
+    name,
+    weak: false,
+    attributes:
+        attributes ??
+        [
+            createAttribute({
+                idMx: `${idMx}-key`,
+                name: keyName,
+                key: true,
+            }),
+        ],
+})
+
+export const createTernarySide = ({
+    idMx,
+    entity,
+    cardinality = '0:N',
+    role,
+}) => ({
+    idMx,
+    cardinality,
+    ...(role ? { role } : {}),
+    entity: { idMx: typeof entity === 'string' ? entity : entity.idMx },
+})
+
+export const createTernaryRelation = ({
+    idMx,
+    name,
+    side1,
+    side2,
+    side3,
+    attributes = [],
+    canHoldAttributes = false,
+    isIdentifying = false,
+}) => ({
+    idMx,
+    name,
+    arity: RELATION_ARITIES.TERNARY,
+    canHoldAttributes,
+    isIdentifying,
+    side1,
+    side2,
+    side3,
+    attributes,
+})
+
+export const createDiagram = ({ entities = [], relations = [], isas = [] } = {}) => ({
+    entities,
+    relations,
+    isas,
+})
+
+export const createTernaryDiagram = ({
+    side1Cardinality = '0:N',
+    side2Cardinality = '0:N',
+    side3Cardinality = '0:N',
+} = {}) => {
+    const entities = [
+        createStrongEntity({
+            idMx: 'entity-asignatura',
+            name: 'Asignatura',
+            keyName: 'id_asignatura',
+        }),
+        createStrongEntity({
+            idMx: 'entity-profesor',
+            name: 'Profesor',
+            keyName: 'id_profesor',
+        }),
+        createStrongEntity({
+            idMx: 'entity-grupo',
+            name: 'Grupo',
+            keyName: 'id_grupo',
+        }),
+    ]
+
+    return createDiagram({
+        entities,
+        relations: [
+            createTernaryRelation({
+                idMx: 'relation-imparte',
+                name: 'Imparte',
+                side1: createTernarySide({
+                    idMx: 'side-asignatura',
+                    cardinality: side1Cardinality,
+                    entity: entities[0],
+                }),
+                side2: createTernarySide({
+                    idMx: 'side-profesor',
+                    cardinality: side2Cardinality,
+                    entity: entities[1],
+                }),
+                side3: createTernarySide({
+                    idMx: 'side-grupo',
+                    cardinality: side3Cardinality,
+                    entity: entities[2],
+                }),
+                attributes: [
+                    createAttribute({
+                        idMx: 'attr-horas',
+                        name: 'horas',
+                    }),
+                ],
+            }),
+        ],
+    })
+}
+
+export const createRepeatedParticipantTernaryDiagram = () => {
+    const entities = [
+        createStrongEntity({
+            idMx: 'entity-tenista',
+            name: 'Tenista',
+            keyName: 'id_tenista',
+        }),
+        createStrongEntity({
+            idMx: 'entity-fecha',
+            name: 'Fecha',
+            keyName: 'fecha',
+        }),
+    ]
+
+    return createDiagram({
+        entities,
+        relations: [
+            createTernaryRelation({
+                idMx: 'relation-juega',
+                name: 'Juega',
+                canHoldAttributes: true,
+                side1: createTernarySide({
+                    idMx: 'side-tenista-local',
+                    cardinality: '0:N',
+                    role: 'tenista local',
+                    entity: entities[0],
+                }),
+                side2: createTernarySide({
+                    idMx: 'side-tenista-visitante',
+                    cardinality: '0:N',
+                    role: 'tenista visitante',
+                    entity: entities[0],
+                }),
+                side3: createTernarySide({
+                    idMx: 'side-fecha',
+                    cardinality: '0:N',
+                    role: 'fecha',
+                    entity: entities[1],
+                }),
+            }),
+        ],
+    })
+}
--- tests/unit/sql/ternaryRelations.test.js
+++ tests/unit/sql/ternaryRelations.test.js
@@ -1,134 +1,16 @@
 import { describe, expect, test } from 'vitest'
-import { RELATION_ARITIES } from '../../../src/domain/er/relations'
 import { generateSQL } from '../../../src/services/sql'
+import {
+    createRepeatedParticipantTernaryDiagram,
+    createTernaryDiagram,
+} from '../../helpers/diagramBuilders'
 import { buildSQLAssertions } from '../../helpers/sqlAssertions'
 
 const { expectSQLToContain } = buildSQLAssertions(expect)
 
-const createEntity = ({ idMx, name, keyName }) => ({
-    idMx,
-    name,
-    weak: false,
-    attributes: [
-        {
-            idMx: `${idMx}-key`,
-            name: keyName,
-            key: true,
-            partialKey: false,
-        },
-    ],
-})
-
-const createTernaryGraph = ({
-    side1Cardinality = '0:N',
-    side2Cardinality = '0:N',
-    side3Cardinality = '0:N',
-} = {}) => {
-    const entities = [
-        createEntity({
-            idMx: 'entity-asignatura',
-            name: 'Asignatura',
-            keyName: 'id_asignatura',
-        }),
-        createEntity({
-            idMx: 'entity-profesor',
-            name: 'Profesor',
-            keyName: 'id_profesor',
-        }),
-        createEntity({
-            idMx: 'entity-grupo',
-            name: 'Grupo',
-            keyName: 'id_grupo',
-        }),
-    ]
-
-    return {
-        entities,
-        relations: [
-            {
-                idMx: 'relation-imparte',
-                name: 'Imparte',
-                arity: RELATION_ARITIES.TERNARY,
-                canHoldAttributes: false,
-                isIdentifying: false,
-                side1: {
-                    idMx: 'side-asignatura',
-                    cardinality: side1Cardinality,
-                    entity: { idMx: entities[0].idMx },
-                },
-                side2: {
-                    idMx: 'side-profesor',
-                    cardinality: side2Cardinality,
-                    entity: { idMx: entities[1].idMx },
-                },
-                side3: {
-                    idMx: 'side-grupo',
-                    cardinality: side3Cardinality,
-                    entity: { idMx: entities[2].idMx },
-                },
-                attributes: [
-                    {
-                        idMx: 'attr-horas',
-                        name: 'horas',
-                        key: false,
-                        partialKey: false,
-                    },
-                ],
-            },
-        ],
-    }
-}
-
-const createRepeatedParticipantTernaryGraph = () => {
-    const entities = [
-        createEntity({
-            idMx: 'entity-tenista',
-            name: 'Tenista',
-            keyName: 'id_tenista',
-        }),
-        createEntity({
-            idMx: 'entity-fecha',
-            name: 'Fecha',
-            keyName: 'fecha',
-        }),
-    ]
-
-    return {
-        entities,
-        relations: [
-            {
-                idMx: 'relation-juega',
-                name: 'Juega',
-                arity: RELATION_ARITIES.TERNARY,
-                canHoldAttributes: true,
-                isIdentifying: false,
-                side1: {
-                    idMx: 'side-tenista-local',
-                    cardinality: '0:N',
-                    role: 'tenista local',
-                    entity: { idMx: entities[0].idMx },
-                },
-                side2: {
-                    idMx: 'side-tenista-visitante',
-                    cardinality: '0:N',
-                    role: 'tenista visitante',
-                    entity: { idMx: entities[0].idMx },
-                },
-                side3: {
-                    idMx: 'side-fecha',
-                    cardinality: '0:N',
-                    role: 'fecha',
-                    entity: { idMx: entities[1].idMx },
-                },
-                attributes: [],
-            },
-        ],
-    }
-}
-
 describe('Ternary relationship SQL generation', () => {
     test('should generate a ternary relation table with a three-column primary key for N:M:P cardinalities', () => {
-        const graph = createTernaryGraph()
+        const graph = createTernaryDiagram()
 
         const sql = generateSQL(graph)
 
@@ -147,7 +29,7 @@ describe('Ternary relationship SQL generation', () => {
     })
 
     test('should keep the maximum-one side outside the primary key and mark it as not null for 1:M:N cardinalities', () => {
-        const graph = createTernaryGraph({
+        const graph = createTernaryDiagram({
             side1Cardinality: '0:1',
             side2Cardinality: '0:N',
             side3Cardinality: '0:N',
@@ -170,7 +52,7 @@ describe('Ternary relationship SQL generation', () => {
     })
 
     test('should render additional candidate keys as table unique constraints for 1:1:N cardinalities', () => {
-        const graph = createTernaryGraph({
+        const graph = createTernaryDiagram({
             side1Cardinality: '0:1',
             side2Cardinality: '0:1',
             side3Cardinality: '0:N',
@@ -194,7 +76,7 @@ describe('Ternary relationship SQL generation', () => {
     })
 
     test('should generate distinct foreign keys for repeated ternary participants', () => {
-        const graph = createRepeatedParticipantTernaryGraph()
+        const graph = createRepeatedParticipantTernaryDiagram()
 
         const sql = generateSQL(graph)
 
