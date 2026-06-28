1f85181e253fd5aa76bcba0d884b71844d16ed10
steven <saa1002@alu.ubu.es>
2026-06-28 11:50:53 +0200

    docs: complete programmer manual appendix

--- src/buildInfo.js
+++ src/buildInfo.js
@@ -1,4 +1,4 @@
 // Archivo generado automáticamente por scripts/generateBuildInfo.js
 export const APP_VERSION = "1.0.0";
-export const BUILD_DATE = "27/06/2026";
-export const BUILD_COMMIT = "54cc454";
+export const BUILD_DATE = "28/06/2026";
+export const BUILD_COMMIT = "ea14fa7";
